5e8b7b660865b59df78b5e047cd762fbe397d50b
Balakrishh <balakrishna.05412@gmail.com>
2015-02-07 21:21:17 -0500

    final

---
+++ src/main/java/com/bala/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.bala;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+public class HelloWorld {
+	public static void main(String[] args ){
+	Spark.get( new Route("/"){
+		@Override
+	public Object handle(final Request arg0,final Response arg1) {
+/*this is my helloworld*/
+		return "Hello Worlds from spark";
+		}
+	});
+	}
+}
