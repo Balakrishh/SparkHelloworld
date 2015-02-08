48f6788f7b4f613f0bb366478e95945196129e1c
Balakrishh <balakrishna.05412@gmail.com>
2015-02-07 22:21:58 -0500

    final commit

--- src/main/java/com/bala/HelloWorld.java
+++ src/main/java/com/bala/HelloWorld.java
@@ -8,7 +8,6 @@ import spark.Spark;
 
 public class HelloWorld {
 	public static void main(String[] args ){
-<<<<<<< HEAD
 	Spark.get( new Route("/"){
 		@Override
 	public Object handle(final Request arg0,final Response arg1) {
@@ -16,10 +15,5 @@ public class HelloWorld {
 		return "Hello Worlds from spark";
 		}
 	});
-=======
-      get("/hello", (request, response) -> {
-         return "Hello World!";
-      });
->>>>>>> 568e24a6dabad2b137ac1313f29dc1ec8da17d08
 	}
 }
